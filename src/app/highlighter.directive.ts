f020b87b2970898c41a6732862cc001d2f87c576
Divij Handa <divij.handa.2010@gmail.com>
2021-01-16 23:24:56 +0530

    Add files via upload

---
+++ src/app/highlighter.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlighter]'
+})
+export class HighlighterDirective {
+
+  constructor(private el: ElementRef) { }
+  @Input() defaultColor: string;
+  @Input('appHighlighter') highlightColor: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || this.defaultColor || 'yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
